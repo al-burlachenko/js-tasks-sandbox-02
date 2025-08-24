2d3915118d14ff0fbfafdfd9276ed6c0cb461806
al-burlachenko <alexandrburl@gmail.com>
2025-08-24 21:41:23 +0300

    adds minor changes and experiments(training) for using js libraries lazysizes, lodash (basicLightbox was used previously)

--- js/script.js
+++ js/script.js
@@ -2,64 +2,62 @@ const galleryRoot = document.querySelector(".gallery");
 import { images } from "./data.js";
 
 if (galleryRoot) {
-  //   console.log(data);
-}
-
-const data = images
-  .map((image) => {
-    const { description, original, preview } = image;
-    return `<li class="gallery-item">
+  const data = images
+    .map((image) => {
+      const { description, original, preview } = image;
+      return `<li class="gallery-item">
   <a class="gallery-link" href="${original}">
     <img
-      class="gallery-image"
-      src="${preview}"
-      data-source="${original}"
+      class="gallery-image lazyload"
+      data-src="${original}"
       alt="${description}"
     />
   </a>
 </li>`;
-  })
-  .join("");
+    })
+    .join("");
+  galleryRoot.insertAdjacentHTML("beforeend", data);
+  // galleryRoot.addEventListener("keydown", (event) => console.log(event.key));
+  // // keydown events only fire on elements that currently have keyboard focus (or on document/window, which always listen).
+  // // Clicking on an <img> inside your gallery doesn’t give the parent .gallery focus.
+  // // Unless you explicitly make .gallery focusable (e.g., with tabindex), it will never receive keydown events.
 
-galleryRoot.insertAdjacentHTML("afterbegin", data);
-// galleryRoot.addEventListener("keydown", (event) => console.log(event.key));
-// // keydown events only fire on elements that currently have keyboard focus (or on document/window, which always listen).
-// // Clicking on an <img> inside your gallery doesn’t give the parent .gallery focus.
-// // Unless you explicitly make .gallery focusable (e.g., with tabindex), it will never receive keydown events.
-
-galleryRoot.addEventListener("click", (event) => {
-  event.preventDefault();
-  if (event.target.classList.contains("gallery-image")) {
-    const instance = basicLightbox.create(`
-        <img src="${event.target.dataset.source}" class="slideshowImage">
+  galleryRoot.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (event.target.classList.contains("gallery-image")) {
+      const instance = basicLightbox.create(`
+        <img src="${event.target.dataset.src}" class="slideshowImage">
 `);
-    document.addEventListener("keydown", closeModal);
-    instance.show();
-    function closeModal(event) {
-      if (event.code === "Escape") {
-        instance.close();
-        document.removeEventListener("keydown", closeModal);
-        return;
+      document.addEventListener("keydown", closeModal);
+      instance.show();
+      function closeModal(event) {
+        if (event.code === "Escape") {
+          instance.close();
+          document.removeEventListener("keydown", closeModal);
+          return;
+        }
       }
     }
-  }
-});
+  });
 
-// galleryRoot.addEventListener("keydown", (event) => console.log(event.key));
-// will work when instance is opened because gallery will get focus but it can be unreliable
-document.addEventListener("keydown", (event) => {
-  console.log(event.code);
-  const currentImage = document.querySelector(".slideshowImage");
+  // galleryRoot.addEventListener("keydown", (event) => console.log(event.key));
+  // will work when instance is opened because gallery will get focus but it can be unreliable
+  document.addEventListener(
+    "keydown",
+    _.throttle((event) => {
+      const currentImage = document.querySelector(".slideshowImage");
 
-  if (currentImage) {
-    const dataArr = images.map((item) => item.original);
-    let index = dataArr.indexOf(currentImage.src);
-    // ArrowRight
-    // ArrowLeft
-    index = event.code === "ArrowRight" ? index + 1 : index;
-    index = event.code === "ArrowLeft" ? index - 1 : index;
-    index = index > dataArr.length - 1 ? 0 : index;
-    index = index < 0 ? dataArr.length - 1 : index;
-    currentImage.src = dataArr[index];
-  }
-});
+      if (currentImage) {
+        const dataArr = images.map((item) => item.original);
+        let index = dataArr.indexOf(currentImage.src);
+        // ArrowRight
+        // ArrowLeft
+        index = event.code === "ArrowRight" ? index + 1 : index;
+        index = event.code === "ArrowLeft" ? index - 1 : index;
+        index = index > dataArr.length - 1 ? 0 : index;
+        index = index < 0 ? dataArr.length - 1 : index;
+        currentImage.src = dataArr[index];
+      }
+    }, 400)
+  );
+}
